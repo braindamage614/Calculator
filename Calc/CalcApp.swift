18772156b46ff1dd04a13811ecddcd8c9976a760
Yuanjun Li <liyuan@pwr-craft.com>
2022-02-07 20:13:24 +0800

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by braindamage on 2022/2/7.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
